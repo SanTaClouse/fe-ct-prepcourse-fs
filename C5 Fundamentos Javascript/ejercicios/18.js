b3ff5c09450bf7d961049c96ba6b5ee1f179d005
santiagoriv06 <santiagoriv06@gmail.com>
2025-07-22 16:12:29 -0300

    Ejercicios Fundamentos de Javascript OK

--- C5 Fundamentos Javascript/ejercicios/18.js
+++ C5 Fundamentos Javascript/ejercicios/18.js
@@ -8,6 +8,29 @@ function esVocal(letra) {
   // "n" ---> "Dato incorrecto"
   // "texto largo" ---> "Dato incorrecto"
   // Tu código:
-}
+  if (letra.length != 1 ) {
+    return 'Dato incorrecto'
+  }
 
+
+  else if (letra === 'e') {
+    return 'Es vocal'
+  }
+  else if (letra === 'i') {
+    return 'Es vocal'
+  }
+    else if (letra === 'o') {
+    return 'Es vocal'
+  }
+    else if (letra === 'u') {
+    return 'Es vocal'
+  }
+    
+  else if (letra === 'a') {
+    return 'Es vocal'
+  }
+  else {
+    return 'Dato incorrecto'
+  }
+}
 module.exports = esVocal;
