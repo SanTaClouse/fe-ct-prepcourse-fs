3d6dc036e513acb45f3d414e0736b7b242eda886
santiagoriv06 <santiagoriv06@gmail.com>
2025-07-24 20:04:09 -0300

    Hasta Homework Ej 19 de bucles con dudas

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,18 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  
+  if (a <= 0 || b <= 0){
+    return 0
+  }
+  else {
+    var mult = a
+    for (var i = a ; i < b ; i++) {
+      mult *= i + 1
+        
+    }
+    return mult
+  }    
 }
 
 module.exports = productoEntreNúmeros;
--- C6 Bucles/ejercicios/19.js
+++ C6 Bucles/ejercicios/19.js
@@ -2,6 +2,12 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+  var sumHasN = 1
+  if (n > 1) {
+    for (var i = 1 ; i < n ; i++){
+      sumHasN += i +1
+  }
+  return sumHasN
 }
-
+}
 module.exports = sumarHastaN;
