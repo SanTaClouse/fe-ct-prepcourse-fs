942bea42d1d4f706acffcbce5968d0a23f9f79da
santiagoriv06 <santiagoriv06@gmail.com>
2025-07-26 10:30:51 -0300

    Callbacks ejercices OK

--- C8 Objects + Callbacks/callbacks/ejercicios/04.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/04.js
@@ -4,6 +4,11 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // NOTA: no debes retornar nada.
    // Tu código:
+   var suma = 0
+   arrayOfNumbers.forEach(x => {
+      suma += x
+   });
+   cb(suma)
 }
 
 module.exports = sumarArray;
--- C8 Objects + Callbacks/callbacks/ejercicios/08.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/08.js
@@ -3,6 +3,14 @@ const buscarElemento = (array, callback) => {
   // Si el elemento no se encuentra, devuelve el mensje "No se encontró el elemento".
   // La función de callback es la encargada de evaluar si el elemento fue encontrado.
   // Tu código:
+  var element 
+  element = array.find((element) => callback(element));
+  if (!element){
+    return "No se encontró el elemento"
+  }
+  else {
+    return element
+  }
 };
 
 module.exports = buscarElemento;
